9075312d0ed990ec196d1ab4b55873e763d15f5e
Dplusg <goriaev.dm.v@gmail.com>
2020-05-18 22:58:53 +0300

    Done

--- src/BracketsTester.php
+++ src/BracketsTester.php
@@ -4,8 +4,32 @@ namespace dgoryaev;
 
 class BracketsTester
 {
+    const ALLOWED_SYMBOLS = ['(', ')', ' ', "\n", "\t", "\r"];
+
     public function test($str): bool
     {
+        if ($this->validateString($str)) {
+            return $this->validateNesting($str);
+        }
+    }
+
+    public function validateString($str): bool
+    {
+        if (preg_match("/[^\n\t\r() ]+/", $str)) {
+            throw new \InvalidArgumentException('String is incorrect');
+        }
+
         return true;
+    }
+
+    public function validateNesting($str): bool
+    {
+        $str = preg_replace("/[ \t\n\r]+/", '', $str);
+
+        while (preg_match('/\(\)/', $str)) {
+            $str = preg_replace('/\(\)/', '', $str);
+        }
+
+        return empty($str);
     }
 }
